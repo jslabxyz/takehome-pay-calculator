676947170c49577f443651b1da0e99edc9d1347b
jslabxyz <jason@jslabs.xyz>
2025-06-03 10:39:18 +0200

    Updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en\" suppressHydrationWarning>
       <head />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
